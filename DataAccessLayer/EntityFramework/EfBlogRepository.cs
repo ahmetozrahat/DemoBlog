f661f2bc523ea61f6e5aebd5b52b6517234a4952
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2023-05-12 21:05:34 +0300

    Blog categories and content added to html.

--- DataAccessLayer/EntityFramework/EfBlogRepository.cs
+++ DataAccessLayer/EntityFramework/EfBlogRepository.cs
@@ -1,15 +1,17 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfBlogRepository: GenericRepository<Blog>, IBlogDal
-    {
-    }
+	public class EfBlogRepository : GenericRepository<Blog>, IBlogDal
+	{
+		public List<Blog> GetListWithCategory()
+		{
+			using var c = new Context();
+			return c.Blogs.Include(x => x.Category).ToList();
+		}
+	}
 }
