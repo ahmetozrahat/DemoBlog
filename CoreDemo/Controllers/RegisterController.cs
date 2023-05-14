38b64687c2e212a55d0c3f5e03ca01b1f451580e
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2023-05-14 21:56:59 +0300

    Registiration logic added.

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager writerManager = new WriterManager(new EfWriterRepository());
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(Writer writer)
+		{
+			writer.WriterStatus = true;
+			writer.WriterAbout = "Deneme Test";
+			writerManager.WriterAdd(writer);
+			return RedirectToAction("Index", "Blog");
+		}
+	}
+}
