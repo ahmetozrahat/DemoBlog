dc9b4fa70f31e27d53901ef26e4e7d46cce998db
Ahmet Özrahat <ahmetozrahat25@gmail.com>
2023-05-15 22:06:16 +0300

    Social media partial added.

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+	public class AboutManager: IAboutService
+	{
+		IAboutDal _aboutDal;
+
+		public AboutManager(IAboutDal aboutDal)
+		{
+			_aboutDal = aboutDal;
+		}
+
+		public List<About> GetList()
+		{
+			return _aboutDal.GetAll();
+		}
+	}
+}
---
+++ CoreDemo/Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class AboutController : Controller
+	{
+		AboutManager aboutManager = new AboutManager(new EfAboutRepository());
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public PartialViewResult SocialMediaAbout()
+		{
+			var values = aboutManager.GetList();
+			return PartialView(values);
+		}
+	}
+}
